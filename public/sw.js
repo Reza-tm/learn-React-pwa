f9dbdb12ce4f9612210bbe873b734a7f8cb9bbaf
Reza-tm <rezatm331@gmail.com>
2021-12-18 04:16:00 +0330

    fix bug

--- public/sw.js
+++ public/sw.js
@@ -9,15 +9,7 @@ self.addEventListener("install", (e) => {
     caches
       .open(CURRENT_CACHE.static)
       .then((cache) =>
-        cache.addAll([
-          "/",
-          "/static/js/bundle.js",
-          "/build/react_devtools_backend.js",
-          "/js/dom.js",
-          "/js/js.js",
-          "/ws",
-          "/sw.js",
-        ])
+        cache.addAll(["/", "/static/js/bundle.js", "/build/react_devtools_backend.js", "/js/dom.js", "/js/js.js", "/sw.js"])
       )
   );
 });
@@ -27,12 +19,14 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then((response) =>
       response
         ? response
-        : fetch(event.request).then((res) =>
-            caches.open(CURRENT_CACHE.dynamic).then((cache) => {
-              cache.put(event.request.url, res);
-              return res;
-            })
-          )
+        : fetch(event.request)
+            .then((res) =>
+              caches.open(CURRENT_CACHE.dynamic).then((cache) => {
+                cache.put(event.request.url, res.clone());
+                return res;
+              })
+            )
+            .catch((err) => {})
     )
   );
 });
